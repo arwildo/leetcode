cd403c2299c09117a13c134e33122bac77dbebb0
Arwildo <arwildo@gmail.com>
2024-10-30 22:11:09 +0900

    feat: improve code

--- contains-duplicate/main.js
+++ contains-duplicate/main.js
@@ -1,15 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
 var containsDuplicate = function(nums) {
-    let set = new Set()
-    for (let i=0; i < nums.length; i++) {
-            console.log(nums[i]);
-                if (set.has(nums[i])) {
-                    return true;
-                }else{
-                    set.add(nums[i]);
-                }
-    }
-    console.log(set);
-    return false;
-};
+   let map = new Map();
 
-console.log(containsDuplicate([1,2,3,1]));
+   for (let i=0; i < nums.length; i++) {
+        if(map.has(nums[i])) {
+            return true;
+        }
+        map.set(nums[i], i);
+   } 
+   return false;
+};

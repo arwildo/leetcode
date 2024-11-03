d0dd0734653e5ccb9b8a0fbb1beece075b0f89b7
Arwildo <arwildo@gmail.com>
2024-11-03 23:35:46 +0900

    feat: solutino with two pointers

---
+++ two-sum-ii-input-array-is-sorted/main_with_two_pointer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let ret = [];
+    let l = 0;
+    let r = numbers.length - 1;
+
+    while (l < r) {
+        let calculation = numbers[l] + numbers[r];
+        if (calculation == target) {
+            ret.push(l+1, r+1);
+            return ret;
+        } else if (calculation > target) {
+            r = r - 1;
+        } else {
+            l = l + 1;
+        }
+    }
+
+    return ret;
+};

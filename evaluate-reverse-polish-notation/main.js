279b9e1ec5e555d0603b0507e56470ebac23c890
Arwildo <arwildo@gmail.com>
2024-11-12 21:48:05 +0900

    feat: solve new question

---
+++ evaluate-reverse-polish-notation/main.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let numbers = [];
+
+    let operator_list = ['+', '-', '*', '/'];
+    let calc = 0;
+
+    for (let i=0; i < tokens.length; i++) {
+        if (operator_list.includes(tokens[i])) {
+            switch (tokens[i]) {
+                case '+':
+                    calc = parseInt(numbers[numbers.length - 2]) + parseInt(numbers[numbers.length - 1]);
+                    numbers.pop();
+                    numbers.pop();
+                    numbers.push(calc);
+                    continue;
+                case '*':
+                    calc = parseInt(numbers[numbers.length - 2]) * parseInt(numbers[numbers.length - 1]);
+                    numbers.pop();
+                    numbers.pop();
+                    numbers.push(calc);
+                    continue;
+                case '-':
+                    calc = parseInt(numbers[numbers.length - 2]) - parseInt(numbers[numbers.length - 1]);
+                    numbers.pop();
+                    numbers.pop();
+                    numbers.push(calc);
+                    continue;
+                case '/':
+                    calc = Math.trunc(parseInt(numbers[numbers.length - 2]) / parseInt(numbers[numbers.length - 1]));
+                    numbers.pop();
+                    numbers.pop();
+                    numbers.push(calc);
+                    continue;
+            }
+        } else {
+            numbers.push(tokens[i]);
+        }
+    }
+    return parseInt(numbers[0]);
+};
